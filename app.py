74fdcdf770c337304f63e3c904e692a39b71b624
pengdahong <2102533303@qq.com>
2021-08-22 10:46:41 +0800

    peter

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, Response
 from frame_base import ret_frame
+import socket
 
-
-def get_frame():
-    image_bytes = ret_frame()
-    return image_bytes
-
+base_add = ('192.168.43.52', 56565)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(base_add)
 
 app = Flask(__name__)
-
-
 @app.route('/')  # 主页
 def index():
     return render_template('index.html')
 
+#   传递帧
+@app.route('/video_feed')  # 这个地址返回视频流响应
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def get_frame():
+    image_bytes = ret_frame(sock)
+    return image_bytes
 
 #   获得帧字节流
 def gen():
@@ -22,18 +28,9 @@ def gen():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
-
-#   传递帧
-@app.route('/video_feed')  # 这个地址返回视频流响应
-def video_feed():
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(
-        host='192.168.101.8',
-        port=5000,
-        debug=True
+        host='0.0.0.0',
+        port=8798
     )
 
--- frame_base.py
+++ frame_base.py
@@ -1,27 +1,22 @@
-import base64
-import socket
+from binascii import a2b_base64
+import binascii
 
-base_add = ('192.168.101.8', 8338)
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(base_add)
-
-
-def ret_frame():
-    #   先接收大小
-    blength, client = sock.recvfrom(5)
-    #   循环接收
-    length_str = blength.decode('utf-8')
-    length = int(length_str)
+def ret_frame(sock):
+    #   先接收片数
+    length, client = sock.recvfrom(50000)  # 阻塞
+    length = int(length.decode('utf-8'))
+    #   循环接收bytes
     i = 0
-    base_data = ''
+    all_data = ''
     while i < length:
-        data, client = sock.recvfrom(50000)  # 阻塞
-        i += 1
-        if data == b"":
+        data_base, client = sock.recvfrom(50000)  # 阻塞
+        if data_base == b"":
             sock.close()
             break
-        data = data.decode('utf-8')
-        base_data += data
+        i += 1
+        #   bytes转string
+        data = data_base.decode('utf-8')
+        all_data += data
     #   循环结束，base64转字节流
-    x = base64.b64decode(base_data)
-    return x
+    #`x = base64.b64decode(all_data)
+    return binascii.a2b_base64(all_data)
